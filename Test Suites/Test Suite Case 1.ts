04c62fef4767dc4d55fb0654bc8e87ec004450ba
RVO <RVO@192.168.1.120>
2018-11-14 12:01:07 +0800

    Commit 3

--- Test Suites/Test Suite Case 1.ts
+++ Test Suites/Test Suite Case 1.ts
@@ -4,7 +4,7 @@
    <name>Test Suite Case 1</name>
    <tag></tag>
    <isRerun>false</isRerun>
-   <lastRun>2018-11-12T17:53:29</lastRun>
+   <lastRun>2018-11-14T11:51:46</lastRun>
    <mailRecipient></mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
